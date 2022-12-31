483323cc8929aed37b8ee10db84290713c12f6db
varunnaik1121 <varunnaik1121@gmail.com>
2022-12-31 10:57:54 +0530

    updated cart btn

--- js/cartLogic.js
+++ js/cartLogic.js
@@ -2,33 +2,17 @@ console.log('hello');
 const increaseBtn = document.querySelectorAll('.increase');
 const decreaseBtn = document.querySelectorAll('.decrease');
 const deleteBtn = document.querySelectorAll('.delete');
-console.log(increaseBtn);
 
-const updateTotal = () => {
-  const allNodes = document.querySelectorAll('.cart-card');
-  allNodes.forEach((eachNode) => {
-    let price =
-      eachNode.children[1].childNodes[3].childNodes[1].children[1].textContent;
-    console.log(price);
-  });
-};
-
-updateTotal();
 increaseBtn.forEach((incBtn) => {
   incBtn.addEventListener('click', (e) => {
     let ele = e.target.parentElement.children[1];
     let value = Number(ele.textContent);
     if (value >= 1) {
       ele.textContent = value += 1;
+      updateTotal();
     } else {
       return;
     }
-    // console.log(value);
-    // if (Number(ele.value) >= 1) {
-
-    // } else {
-    //   return;
-    // }
   });
 });
 
@@ -38,27 +22,23 @@ decreaseBtn.forEach((decBtn) => {
     let value = Number(ele.textContent);
     if (value >= 2) {
       ele.textContent = value -= 1;
+      updateTotal();
     } else {
       return;
     }
-    // console.log(value);
-    // if (Number(ele.value) >= 1) {
-
-    // } else {
-    //   return;
-    // }
   });
 });
+
+const updateCartTotal = () => {
+  var cartItemContainer = document.getElementsByClassName('cart-container')[0];
+  console.log(cartItemContainer);
+  var cartRows;
+};
+updateCartTotal();
 
 deleteBtn.forEach((btn) => {
   console.log(btn);
   btn.addEventListener('click', (e) => {
-    // let parent = e.target.parentElement;
-    // let grandParent = parent.parentElement;
-    // console.log(grandParent);
     e.currentTarget.parentElement.remove();
-
-    // updateCards();
-    // updateTotal();
   });
 });
