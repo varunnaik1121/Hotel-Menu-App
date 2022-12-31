16a7c1a4a3b4d099ea0e21faf2c7910ce922b347
varunnaik1121 <varunnaik1121@gmail.com>
2022-12-31 12:09:09 +0530

    updated index

--- js/cartLogic.js
+++ js/cartLogic.js
@@ -3,13 +3,45 @@ const increaseBtn = document.querySelectorAll('.increase');
 const decreaseBtn = document.querySelectorAll('.decrease');
 const deleteBtn = document.querySelectorAll('.delete');
 
+if (
+  document
+    .querySelector('.cart-container')
+    .children[0].classList.contains('cart-card') === false
+) {
+  document.querySelector('.cart-container').innerHTML = `<div class="empty-cart-container">
+  <h5 class="dish-title">Cart Is Empty</h5>
+  <a href="./TodaysMenuPage.html" class="empty">Add Items</a>
+  </div>`;
+}
+
+const updateCartTotal = () => {
+  let total = 0;
+  if (document.getElementsByClassName('cart-container')[0].childNodes) {
+    var cartItemContainer =
+      document.getElementsByClassName('cart-container')[0];
+    console.log(cartItemContainer);
+    var cartRows = document.querySelectorAll('.cart-card');
+    console.log(cartRows);
+    cartRows.forEach((row) => {
+      var priceTag = row.querySelector('.price');
+      var quantityTag = row.querySelector('.cart-value');
+      total =
+        total +
+        parseFloat(priceTag.innerText) * parseFloat(quantityTag.innerText);
+    });
+  }
+  document.querySelector('.total-amount').innerHTML = total;
+  console.log(total);
+};
+window.addEventListener('DOMContentLoaded', updateCartTotal);
+
 increaseBtn.forEach((incBtn) => {
   incBtn.addEventListener('click', (e) => {
     let ele = e.target.parentElement.children[1];
     let value = Number(ele.textContent);
     if (value >= 1) {
       ele.textContent = value += 1;
-      updateTotal();
+      updateCartTotal();
     } else {
       return;
     }
@@ -22,23 +54,17 @@ decreaseBtn.forEach((decBtn) => {
     let value = Number(ele.textContent);
     if (value >= 2) {
       ele.textContent = value -= 1;
-      updateTotal();
+      updateCartTotal();
     } else {
       return;
     }
   });
 });
 
-const updateCartTotal = () => {
-  var cartItemContainer = document.getElementsByClassName('cart-container')[0];
-  console.log(cartItemContainer);
-  var cartRows;
-};
-updateCartTotal();
-
 deleteBtn.forEach((btn) => {
   console.log(btn);
   btn.addEventListener('click', (e) => {
     e.currentTarget.parentElement.remove();
+    updateCartTotal();
   });
 });
